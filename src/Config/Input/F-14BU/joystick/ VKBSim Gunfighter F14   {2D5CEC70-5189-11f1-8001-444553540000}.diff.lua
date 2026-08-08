6f0b2ab4d258f6b91d44b644bed4e91e754d598e
scottymac-cisco2 <scottymacdev@icloud.com>
2026-08-07 17:53:32 -0700

    Make VKB BTN7 the HOTAS shift modifier

--- src/Config/Input/F-14BU/joystick/ VKBSim Gunfighter F14   {2D5CEC70-5189-11f1-8001-444553540000}.diff.lua
+++ src/Config/Input/F-14BU/joystick/ VKBSim Gunfighter F14   {2D5CEC70-5189-11f1-8001-444553540000}.diff.lua
@@ -2,134 +2,48 @@ local diff = {
 	["axisDiffs"] = {
 		["a2001cdnil"] = {
 			["name"] = "Pitch",
-			["removed"] = {
-				[1] = {
-					["key"] = "JOY_Y",
-				},
-			},
+			["removed"] = { [1] = { ["key"] = "JOY_Y" } },
 		},
 		["a2002cdnil"] = {
 			["name"] = "Roll",
-			["removed"] = {
-				[1] = {
-					["key"] = "JOY_X",
-				},
-			},
+			["removed"] = { [1] = { ["key"] = "JOY_X" } },
 		},
 		["a2003cdnil"] = {
 			["name"] = "Rudder",
-			["removed"] = {
-				[1] = {
-					["key"] = "JOY_RZ",
-				},
-			},
+			["removed"] = { [1] = { ["key"] = "JOY_RZ" } },
 		},
 		["a2004cdnil"] = {
 			["name"] = "Throttle Both",
-			["removed"] = {
-				[1] = {
-					["key"] = "JOY_Z",
-				},
-			},
+			["removed"] = { [1] = { ["key"] = "JOY_Z" } },
 		},
 		["a3083cd57"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_RX",
-				},
-			},
+			["added"] = { [1] = { ["key"] = "JOY_RX" } },
 			["name"] = "DLC / Maneuver Flaps",
 		},
 	},
 	["keyDiffs"] = {
-		["dnilp2019u2019cdnilvdnilvp-1vu0"] = {
-			["added"] = { [1] = { ["key"] = "JOY_BTN11" } },
-			["name"] = "Trim Pitch Down",
-		},
-		["dnilp2019u2019cdnilvdnilvp1vu0"] = {
-			["added"] = { [1] = { ["key"] = "JOY_BTN10" } },
-			["name"] = "Trim Pitch Up",
-		},
-		["dnilp2020u2020cdnilvdnilvp-1vu0"] = {
-			["added"] = { [1] = { ["key"] = "JOY_BTN9" } },
-			["name"] = "Trim Roll Left Wing Down",
-		},
-		["dnilp2020u2020cdnilvdnilvp1vu0"] = {
-			["added"] = { [1] = { ["key"] = "JOY_BTN12" } },
-			["name"] = "Trim Roll Right Wing Down",
-		},
-		["d3023pnilunilcd18vd1vpnilvunil"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_BTN6",
-				},
-			},
-			["name"] = "Catapult Salute",
-		},
+		["dnilp2019u2019cdnilvdnilvp-1vu0"] = { ["added"] = { [1] = { ["key"] = "JOY_BTN11" } }, ["name"] = "Trim Pitch Down" },
+		["dnilp2019u2019cdnilvdnilvp1vu0"] = { ["added"] = { [1] = { ["key"] = "JOY_BTN10" } }, ["name"] = "Trim Pitch Up" },
+		["dnilp2020u2020cdnilvdnilvp-1vu0"] = { ["added"] = { [1] = { ["key"] = "JOY_BTN9" } }, ["name"] = "Trim Roll Left Wing Down" },
+		["dnilp2020u2020cdnilvdnilvp1vu0"] = { ["added"] = { [1] = { ["key"] = "JOY_BTN12" } }, ["name"] = "Trim Roll Right Wing Down" },
+		["d3023pnilunilcd18vd1vpnilvunil"] = { ["added"] = { [1] = { ["key"] = "JOY_BTN6" } }, ["name"] = "Catapult Salute" },
 		["d3085pnilu3085cd57vd1vpnilvu0"] = {
 			["added"] = {
 				[1] = {
-					["key"] = "JOY_BTN7",
+					["key"] = "JOY_BTN5",
+					["reformers"] = { [1] = "JOY_BTN7" },
 				},
 			},
+			["removed"] = { [1] = { ["key"] = "JOY_BTN7" } },
 			["name"] = "Autopilot Reference / Nosewheel Steering Toggle",
 		},
-		["d3078pnilu3078cd57vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_BTN3",
-				},
-			},
-			["name"] = "Store Release",
-		},
-		["d3079pnilunilcd55vd-0.5vpnilvunil"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_BTN14",
-				},
-			},
-			["name"] = "Weapon Selector Sidewinder Missiles",
-		},
-		["d3079pnilunilcd55vd-1vpnilvunil"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_BTN16",
-				},
-			},
-			["name"] = "Weapon Selector Off",
-		},
-		["d3079pnilunilcd55vd0.5vpnilvunil"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_BTN13",
-				},
-			},
-			["name"] = "Weapon Selector Sparrow or Phoenix Missiles",
-		},
-		["d3079pnilunilcd55vd1vpnilvunil"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_BTN15",
-				},
-			},
-			["name"] = "Weapon Selector Gun",
-		},
-		["d3084pnilu3084cd57vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_BTN5",
-				},
-			},
-			["name"] = "DLC Toggle / Countermeasure Dispense",
-		},
-		["d3087pnilu3087cd57vd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_BTN1",
-				},
-			},
-			["name"] = "Trigger",
-		},
+		["d3078pnilu3078cd57vd1vpnilvu0"] = { ["added"] = { [1] = { ["key"] = "JOY_BTN3" } }, ["name"] = "Store Release" },
+		["d3079pnilunilcd55vd-0.5vpnilvunil"] = { ["added"] = { [1] = { ["key"] = "JOY_BTN14" } }, ["name"] = "Weapon Selector Sidewinder Missiles" },
+		["d3079pnilunilcd55vd-1vpnilvunil"] = { ["added"] = { [1] = { ["key"] = "JOY_BTN16" } }, ["name"] = "Weapon Selector Off" },
+		["d3079pnilunilcd55vd0.5vpnilvunil"] = { ["added"] = { [1] = { ["key"] = "JOY_BTN13" } }, ["name"] = "Weapon Selector Sparrow or Phoenix Missiles" },
+		["d3079pnilunilcd55vd1vpnilvunil"] = { ["added"] = { [1] = { ["key"] = "JOY_BTN15" } }, ["name"] = "Weapon Selector Gun" },
+		["d3084pnilu3084cd57vd1vpnilvu0"] = { ["added"] = { [1] = { ["key"] = "JOY_BTN5" } }, ["name"] = "DLC Toggle / Countermeasure Dispense" },
+		["d3087pnilu3087cd57vd1vpnilvu0"] = { ["added"] = { [1] = { ["key"] = "JOY_BTN1" } }, ["name"] = "Trigger" },
 	},
 }
 return diff
